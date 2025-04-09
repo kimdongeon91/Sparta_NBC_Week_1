291095031c09bc3a3d53b9258c9499f0d550b84f
kimdongeon91 <kimdongeon1991@gmail.com>
2025-04-09 11:43:12 +0900

    feat : 시간이 30초부터 시작해서 0초가 되면 정지하는 기능입니다.
    - 시간이 30초부터 시작해서 0초가 되면 정지하는 기능입니다.

--- Scripts/GameManager.cs
+++ Scripts/GameManager.cs
@@ -7,7 +7,7 @@ public class GameManager : MonoBehaviour
 {
     public Text timeTxt;
 
-    float time = 0.0f;
+    float time = 30.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,7 +18,14 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        time += Time.deltaTime;
-        timeTxt.text = time.ToString("N2");
+        if (time > 0f)
+        {
+            time -= Time.deltaTime;
+            timeTxt.text = time.ToString("N2");
+        }
+        else
+        {
+            Time.timeScale = 1.0f;
+        }
     }
 }
