afd3e5774c0e981a27b23713886eb1649ac3cebb
kimdongeon91 <kimdongeon1991@gmail.com>
2025-04-09 14:42:10 +0900

    feat : 배열을 랜덤하게 정렬하는 기능입니다.
    - 배열을 랜덤하게 정렬하는 기능입니다.

--- Scripts/Board.cs
+++ Scripts/Board.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class Board : MonoBehaviour
 {
@@ -9,6 +10,9 @@ public class Board : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        int[] arr = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5 };
+        arr = arr.OrderBy(x => Random.Range(0f, 5f)).ToArray();
+
         for(int i = 0; i < 12; i++)
         {
             GameObject go = Instantiate(card, this.transform);
